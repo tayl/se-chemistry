3e36309bc6dc5042189a5a050438331dd6bfabe3
ufcama <ufcama@yahoo.com>
2016-03-24 15:37:14 -0400

    Chem list view with array
    data is in array instead of cvs file

---
+++ ChemWorking/app/src/main/java/example/com/chem/Product.java
@@ -0,0 +1,53 @@
+package example.com.chem;
+
+/**
+ * Created by robertvalladares on 3/14/16.
+ */public class Product {
+    private int id;
+    private String name;
+    private double price;
+    private String description;
+
+    //Constructor
+
+    public Product(int id, String name, double price, String description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    //Setter, getter
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ ChemWorking/app/src/main/java/example/com/chem/ProductListAdapter.java
@@ -0,0 +1,57 @@
+package example.com.chem;
+
+/**
+ * Created by robertvalladares on 3/14/16.
+ */
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ProductListAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private List<Product> mProductList;
+
+    //Constructor
+
+    public ProductListAdapter(Context mContext, List<Product> mProductList) {
+        this.mContext = mContext;
+        this.mProductList = mProductList;
+    }
+
+    @Override
+    public int getCount() {
+        return mProductList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mProductList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = View.inflate(mContext, R.layout.item_product_list, null);
+        TextView tvName = (TextView)v.findViewById(R.id.tv_name);
+        TextView tvPrice = (TextView)v.findViewById(R.id.tv_price);
+        TextView tvDescription = (TextView)v.findViewById(R.id.tv_description);
+        //Set text for TextView
+        tvName.setText(mProductList.get(position).getName());
+        tvPrice.setText(String.valueOf(mProductList.get(position).getPrice()) + "g");
+        tvDescription.setText(mProductList.get(position).getDescription());
+
+        //Save product id to tag
+        v.setTag(mProductList.get(position).getId());
+
+        return v;
+    }
+}
