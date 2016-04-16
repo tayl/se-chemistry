a226af43c6d155536ce4d9271bbc684d22399382
Taylor Veith <taylor.veith@gmail.com>
2016-04-16 15:21:16 -0400

    fade in on splash screen, image updated as well

--- tayl/app/src/main/java/activities/SplashScreen.java
+++ tayl/app/src/main/java/activities/SplashScreen.java
@@ -3,6 +3,9 @@ package activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.chemistry.R;
 
@@ -13,11 +16,15 @@ public class SplashScreen extends Activity {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+        
+        ImageView imageView = (ImageView) findViewById(R.id.splashimageview);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.fade);
+        imageView.startAnimation(animation);
 
         Thread timerThread = new Thread() {
             public void run() {
                 try {
-                    sleep(1500L);
+                    sleep(2000L);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
